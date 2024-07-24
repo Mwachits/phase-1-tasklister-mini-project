d55b83328c91ccd3a8d7ec65a363137d4b160495
David Karauni <davymwachits@gmail.com>
2024-07-24 16:49:57 +0300

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+  const taskList = document.getElementById("tasks");
+
+  form.addEventListener("submit", function(event) {
+    event.preventDefault(); 
+
+    
+    const taskInput = document.getElementById("new-task-description");
+    const taskText = taskInput.value.trim();
+
+    if (taskText !== "") {
+      addTask(taskText); 
+      taskInput.value = ""; 
+    }
+  });
+
+  function addTask(taskText) {
+  
+    const newTask = document.createElement("li");
+    newTask.textContent = taskText;
+
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "x";
+    deleteButton.addEventListener("click", function() {
+      taskList.removeChild(newTask); 
+    });
+
+    newTask.appendChild(deleteButton);
+
+    taskList.appendChild(newTask);
+  }
 });
